212862f16504e7e91336232bc8d69c54815b77ae
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-07-31 15:36:49 -0700

    simplestorage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,9 +3,26 @@
 pragma solidity ^0.6.0;
 
 contract SimpleStorage {
-    uint256 favoriteNumber =5;
+    uint256 public favoriteNumber;
     
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //Arrays
+    People[] public people;
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+// adding people to the array
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People({favoriteNumber:_favoriteNumber, name: _name}));
+    }
+
 }
